1a17ae2aeeb90490559a0006fe35bfa4a0281db2
Krizel Rika <krizelcodes@gmail.com>
2026-01-20 22:08:22 +0800

    IBM lesson Module 3: added new function and data: findBySpecialization and added specialization.

--- employee_details.js
+++ employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Java' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Ruby' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Angular' },
     //... More employee records can be added here
 ];
 
@@ -19,7 +19,10 @@ function calculateTotalSalaries() {
 
 function displayHREmployees() {
     const hrEmployees = employees.filter(employee => employee.department === 'HR');
-    document.getElementById('employeesDetails').innerHTML = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML =
+        hrEmployees.map((employee, index) =>
+            `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`
+        ).join('');
 }
 
 function findEmployeeById(employeeId) {
@@ -29,5 +32,21 @@ function findEmployeeById(employeeId) {
     }
     else{
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+    }
+}
+
+function findBySpecialization(specialization) {
+    const foundEmployees = employees.filter(
+        employee => employee.specialization === specialization
+    );
+
+    if (foundEmployees.length > 0) {
+        document.getElementById('employeesDetails').innerHTML =
+            foundEmployees.map(employee =>
+                `<p>${employee.id}: ${employee.name} - ${employee.specialization}</p>`
+            ).join('');
+    } else {
+        document.getElementById('employeesDetails').innerHTML =
+            "No such employee has been found with this specialization";
     }
 }
